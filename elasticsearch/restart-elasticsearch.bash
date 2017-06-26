3490988f7699589250bf886d9470f3500b77b648
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2017-06-26 07:49:52 +0200

    restatr elasticsearch and misc

--- elasticsearch/restart-elasticsearch.bash
+++ elasticsearch/restart-elasticsearch.bash
@@ -3,10 +3,10 @@ set -e
 
 es_server_prefix=elastic10
 es_server_suffix=.eqiad.wmnet
-first_server_index=26
-nb_of_servers_in_cluster=36
+first_server_index=52
+nb_of_servers_in_cluster=52
 
-icinga=tegmen.wikimedia.org
+icinga=einsteinium.wikimedia.org
 curator="curator --config /etc/curator/config.yaml"
 
 for i in $(seq -w ${first_server_index} ${nb_of_servers_in_cluster}); do
@@ -16,8 +16,10 @@ for i in $(seq -w ${first_server_index} ${nb_of_servers_in_cluster}); do
     echo "make sure cluster is green before we start"
     ssh ${server} "until curl -s 127.0.0.1:9200/_cat/health | grep green; do echo -n .; sleep 10; done"
 
-    echo "ready to start restart ${server}, press [enter] to start"
-    read
+    echo "waiting for all relocation to stabilize"
+    ssh ${server} "while curl -s 127.0.0.1:9200/_cat/recovery | grep -v done; do echo -n .; sleep 10; done"
+
+    echo "ready to start restart ${server}"
 
     echo "disabling replication"
     ssh ${server} sudo ${curator} /etc/curator/disable-shard-allocation.yaml
@@ -26,19 +28,10 @@ for i in $(seq -w ${first_server_index} ${nb_of_servers_in_cluster}); do
     ssh ${server} curl -s -XPOST '127.0.0.1:9200/_flush/synced?pretty'
 
     echo "disabling icinga alerts for ${hostname}"
-    ssh ${icinga} sudo icinga-downtime -h ${hostname} -d 600 -r "upgrade in progress"
-
-    echo "disable puppet on ${server}"
-    ssh ${server} sudo puppet agent --disable "upgrade in progress"
+    ssh ${icinga} sudo icinga-downtime -h ${hostname} -d 1200 -r "restart in progress"
 
     echo "depooling ${hostname}"
     ssh ${server} sudo -i /usr/local/bin/depool
-
-    echo "stop elasticsearch"
-    ssh ${server} sudo service elasticsearch stop
-
-    echo "upgrade elasticsearch"
-    ssh ${server} sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install elasticsearch
 
     echo "rebooting server"
     ssh ${server} "sudo nohup reboot &> /dev/null & exit"
@@ -64,10 +57,6 @@ for i in $(seq -w ${first_server_index} ${nb_of_servers_in_cluster}); do
 
     echo "pooling ${hostname}"
     ssh ${server} sudo -i /usr/local/bin/pool
-
-    echo "re-enabling puppet on ${server}"
-    ssh ${server} sudo puppet agent --enable
-
 
     echo "waiting for cluster recovery"
     ssh ${server} "until curl -s 127.0.0.1:9200/_cat/health | grep green; do echo -n .; sleep 10; done"
