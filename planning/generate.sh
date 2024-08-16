61109b1cdaf3d0e9fb20e68032662596286cbb20
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2024-08-16 17:11:40 +0200

    airflow migration planning

--- planning/generate.sh
+++ planning/generate.sh
@@ -2,6 +2,8 @@
 
 project=$1
 
+mkdir -p output/${project}
+
 while inotifywait -e MODIFY ${project}.tjp; do
   tj3 -o output/${project} ${project}.tjp
   rsync -rav output/${project} people2003.codfw.wmnet:~/public_html/planning
