19f5bc9e278757ba9baa2a02ce3f362a8dab202b
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2025-01-24 15:24:21 +0100

    add all q3 goals for SRE and Search, improved generate.sh script

--- planning/generate.sh
+++ planning/generate.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
-project=$1
+projects="q3-dpe q3-search q3-sre"
 
 mkdir -p output/${project}
 
-while inotifywait -e MODIFY ${project}.tjp; do
-  tj3 -o output/${project} ${project}.tjp
-  rsync -rav output/${project} people2003.codfw.wmnet:~/public_html/planning
+while inotifywait -e MODIFY -r .; do
+  for project in ${projects}; do
+    mkdir -p output/${project}
+    tj3 -o output/${project} ${project}.tjp
+  done
+  rsync -rav output/* people2003.codfw.wmnet:~/public_html/planning
 done
-
