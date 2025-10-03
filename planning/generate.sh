fa64a77c28c589041006f2d61cbdd1ed65a4a1e2
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2025-10-03 17:58:54 +0200

    sre planning q2

--- planning/generate.sh
+++ planning/generate.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env sh
 
-projects="q3-dpe q3-search q3-sre"
-
-mkdir -p output/${project}
+projects="FY2025-2026-q2-sre"
 
 while inotifywait -e MODIFY -r .; do
   for project in ${projects}; do
     mkdir -p output/${project}
     tj3 -o output/${project} ${project}.tjp
   done
-  rsync -rav output/* people2003.codfw.wmnet:~/public_html/planning
+  rsync -rav output/* people2004.codfw.wmnet:~/public_html/planning
 done
